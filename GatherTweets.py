f4d5696b441184f17c6b0abf41b953e4dd178f6b
Elisabeth Paulson <elisabethpaulson@C02PJ1FVG3QP.local>
2015-09-08 13:10:44 -0400

    Update files

--- GatherTweets.py
+++ GatherTweets.py
@@ -1,115 +1,167 @@
 __author__ = 'elisabethpaulson'
-
-##### THIS FILE GATHERS TWEETS BASED ON KEYWORDS "FEMA" "HURRICANE" "FLOOD" AND "EMERGENCY" ##########
-
-import tweepy
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 from textblob import TextBlob
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk, streaming_bulk
-from pprint import pprint
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+st=SnowballStemmer('english')
+import datetime
+#from TwitterFEMA import *
 
+# import twitter keys and tokens
+from config import *
+
+# create instance of elasticsearch
 es=Elasticsearch()
+#es = Elasticsearch('http://127.0.0.1:9200')
+# es = Elasticsearch(
+#     ['localhost', 'otherhost'],
+#     http_auth=('user', 'secret'),
+#     port=443,
+#     use_ssl=True,
+#     verify_certs=True,
+#     ca_certs=certifi.where(),
+# )
 
-auth = tweepy.OAuthHandler('lRBqclAO9jIuVn1i2GyamBDCI', 'V9CIUsTUU9OFrEM3DgEaKTZjfgrrnNfTG7zMWRrDjWT0Rrf9rt')
-auth.set_access_token('341804649-NCg92iUdPM2XiEvzIOqW4YF89jkMxO8AtZRj0KWK', 'AnAQRpJRaXNZgefmN1xCvc6cLyjJaH0xH0qMsj8Qh4IHz')
-api=tweepy.API(auth)
-pprint(api.rate_limit_status())
-public_tweets=[status for status in tweepy.Cursor(api.search, q='FEMA',lan='english').items(1000)]
-m=0
-for tweet in public_tweets:
-    #print(tweet)
-    datablob=TextBlob(tweet.text)
-    #print datablob
-    #print datablob.sentiment.polarity
-    if datablob.sentiment.polarity < 0:
-        sentiment = "negative"
-    elif datablob.sentiment.polarity == 0:
-        sentiment = "neutral"
-    else:
-        sentiment = "positive"
-    # output sentiment
-    doc={"date": tweet.created_at,
-                   "message": tweet.text,
-                   "polarity": datablob.sentiment.polarity,
-                   "subjectivity": datablob.sentiment.subjectivity,
-                   "sentiment": sentiment}
-    es.index(index="sentiment",
-             doc_type="FEMAtweets",
-                 id=m,
-             body=doc)
-    m=m+1
+class TweetStreamListener(StreamListener):
 
-public_tweets=[status for status in tweepy.Cursor(api.search, q='hurricane',lan='english').items(1000)]
-n=0
-for tweet in public_tweets:
-    #print(tweet)
-    datablob=TextBlob(tweet.text)
-    #print datablob
-    #print datablob.sentiment.polarity
-    if datablob.sentiment.polarity < 0:
-        sentiment = "negative"
-    elif datablob.sentiment.polarity == 0:
-        sentiment = "neutral"
-    else:
-        sentiment = "positive"
-    # output sentiment
-    doc={"date": tweet.created_at,
-                   "message": tweet.text,
-                   "polarity": datablob.sentiment.polarity,
-                   "subjectivity": datablob.sentiment.subjectivity,
-                   "sentiment": sentiment}
-    es.index(index="sentiment",
-             doc_type="hurricanetweets",
-                 id=n,
-             body=doc)
-    n=n+1
+    # on success
+    def on_data(self, data):
+        # decode json
+        dict_data = json.loads(data)
+        text=dict_data["text"]
+        text=text.lower()
+        text=re.sub(r'rt ','',text)
+        text=re.sub(r'http.*','',text)
+        text=re.sub(r'[^a-z ]','',text)
+        #text=re.sub(r'appli[a-z]*',r'apply',text)
+        text=nltk.word_tokenize(text)
+        text=[word for word in text if word.lower() not in stopwords.words("english")]
+        #text=[st.stem(word) for word in text]
+        text=' '.join(text)
+        # pass tweet into TextBlob
+        tweet = TextBlob(dict_data["text"])
+        print text
+        # output sentiment polarity
 
-public_tweets=[status for status in tweepy.Cursor(api.search, q='flood',lan='english').items(1000)]
-q=0
-for tweet in public_tweets:
-    #print(tweet)
-    datablob=TextBlob(tweet.text)
-    #print datablob
-    #print datablob.sentiment.polarity
-    if datablob.sentiment.polarity < 0:
-        sentiment = "negative"
-    elif datablob.sentiment.polarity == 0:
-        sentiment = "neutral"
-    else:
-        sentiment = "positive"
-    # output sentiment
-    doc={"date": tweet.created_at,
-                   "message": tweet.text,
-                   "polarity": datablob.sentiment.polarity,
-                   "subjectivity": datablob.sentiment.subjectivity,
-                   "sentiment": sentiment}
-    es.index(index="sentiment",
-             doc_type="floodtweets",
-                 id=q,
-             body=doc)
-    q=q+1
+        # determine if sentiment is positive, negative, or neutral
+        if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
 
-public_tweets=[status for status in tweepy.Cursor(api.search, q='emergency',lan='english').items(1000)]
-r=0
-for tweet in public_tweets:
-    #print(tweet)
-    datablob=TextBlob(tweet.text)
-    #print datablob
-    #print datablob.sentiment.polarity
-    if datablob.sentiment.polarity < 0:
-        sentiment = "negative"
-    elif datablob.sentiment.polarity == 0:
-        sentiment = "neutral"
-    else:
-        sentiment = "positive"
-    # output sentiment
-    doc={"date": tweet.created_at,
-                   "message": tweet.text,
-                   "polarity": datablob.sentiment.polarity,
-                   "subjectivity": datablob.sentiment.subjectivity,
-                   "sentiment": sentiment}
-    es.index(index="sentiment",
-             doc_type="emergencytweets",
-                 id=r,
-             body=doc)
-    r=r+1
+        # output sentiment
+
+        # medias = dict_data['entities']['urls']
+        # print medias[0]['expanded_url']
+        # add text and sentiment info to elasticsearch
+        es.index(index='temp',
+                 doc_type='temp',
+                 id=1,
+                 refresh=True,
+                 body={
+                     "message": text
+                 })
+        result=es.search_exists(
+            index='temp',
+            doc_type='temp',
+            body={
+                'query':{
+                    #'query_string':{
+                    #    'default_field':'message',
+                    #    'query':'small business', 'loan',' pay budget debt money save spend invest tax dollar apply application paid credit interest bank debtor repay borrow lend lender federal job'
+                    #},
+                    'bool':{
+                        'must_not':[
+                            {
+                            'term':{'message':'student'}
+                            }
+                        ],
+                        'should':[
+                            {
+                            'terms':{
+                                'message':['small business','small businesses','pay','budget','debt','money', 'save','spend','invest','tax','dollar','apply','application','paid','credit','interest','bank','debtor','repay','borrow','lend','lender','federal']}
+                            }
+                            ],
+                        'minimum_should_match':1
+                    }
+                }
+            }
+        )
+
+
+
+        if result==True:
+            print "MATCH!"
+
+            # # Now we have to chunk it and add it to the corpus, and label it according to a topic
+            # chunk=nltk.word_tokenize(text)
+            # chunk=[word for word in chunk if len(word)>2]
+            # chunk=nltk.pos_tag(chunk)
+            # chunkedtext=[]
+            # result=NPchunker.parse(chunk)
+            # #print result
+            # for phrase in result:
+            #     try:
+            #         phrase.label()
+            #         string=''
+            #         m=0
+            #         for word in phrase:
+            #             if m==0:
+            #                 string+=word[0]
+            #                 m+=1
+            #             else: string+=' '+word[0]
+            #         chunkedtext.append(string)
+            #     except: pass
+            #
+            # #dictionary2=corpora.Dictionary.add_documents(corpora.Dictionary(chunkedtext))
+            # #dictionary2.save("GLtweetschunked.dict")
+            # new_vec=dictionary2.doc2bow(chunkedtext)
+            # print "1"
+            # new_tfidf=tfidf2[new_vec]
+            # print "2"
+            # lda2.update(new_tfidf)
+            # print "3"
+            # print lda2[new_tfidf]
+            # print '4'
+            #
+            # print lda2.get_document_topics(new_tfidf,minimum_probability=.7)
+
+            es.index(index="stream",
+                     doc_type="SBL",
+                     body={"user": dict_data["user"]["screen_name"],
+                           'date': datetime.datetime.now(),
+                           "message": text,
+                           # "url": dict_data["urls"]["expanded_url"],
+                           "polarity": tweet.sentiment.polarity,
+                           "subjectivity": tweet.sentiment.subjectivity,
+                           "sentiment": sentiment})
+
+
+        return True
+
+    # on failure
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    # create instance of the tweepy tweet stream listener
+    listener = TweetStreamListener()
+
+    # set twitter keys/tokens
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # create instance of the tweepy stream
+    stream = Stream(auth, listener)
+
+    # search twitter for "congress" keyword
+    stream.filter(track=['small business','small businesses','SBA'])
